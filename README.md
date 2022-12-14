e7152a018b6f4b6936138a3d420e55f978f226a4
l0ulou59 <106763258+l0ulou59@users.noreply.github.com>
2022-12-14 15:48:11 +0100

    Fichiers initiaux pour un projet MakeCode

---
+++ README.md
@@ -0,0 +1,33 @@
+ 
+
+
+> Ouvrir cette page à [https://l0ulou59.github.io/flappy-norsys/](https://l0ulou59.github.io/flappy-norsys/)
+
+## Utiliser comme extension
+
+Ce dépôt peut être ajouté en tant qu'**extension** dans MakeCode.
+
+* ouvrir [https://arcade.makecode.com/](https://arcade.makecode.com/)
+* cliquez sur **Nouveau projet**
+* cliquez sur **Extensions** dans le menu engrenage
+* recherchez **https://github.com/l0ulou59/flappy-norsys** et importez
+
+## Éditer ce projet ![Badge du statut de la compilation](https://github.com/l0ulou59/flappy-norsys/workflows/MakeCode/badge.svg)
+
+Éditer ce dépôt dans MakeCode.
+
+* ouvrir [https://arcade.makecode.com/](https://arcade.makecode.com/)
+* cliquez sur **Importer** puis cliquez sur **Importer l'URL **
+* collez **https://github.com/l0ulou59/flappy-norsys** et cliquez sur importer
+
+## Aperçu des blocs
+
+Cette section montre le code des blocs du dernier commit dans la branche master.
+Cette image peut prendre quelques minutes pour être actualisée.
+
+![Un rendu de la vue des blocs](https://github.com/l0ulou59/flappy-norsys/raw/master/.github/makecode/blocks.png)
+
+#### Métadonnées (utilisées pour la recherche, le rendu)
+
+* for PXT/arcade
+<script src="https://makecode.com/gh-pages-embed.js"></script><script>makeCodeRender("{{ site.makecode.home_url }}", "{{ site.github.owner_name }}/{{ site.github.repository_name }}");</script>
---
+++ images.g.ts
@@ -0,0 +1,139 @@
+// Code généré automatiquement. Ne pas modifier.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "9$M0u0WMa8jR)S%YagO/":
+            case "norsys1":return img`
+7 7 f f f f f f f f f f f f 7 7 
+7 f f f f 8 8 8 8 8 f f f f f 7 
+7 f f f 8 f f f f f 8 f f f f f 
+f f f f 8 f f f f f 8 f f f f f 
+f f 7 7 7 7 7 f 2 2 2 2 2 f f f 
+f 7 f f 8 f f 7 f f 8 f f 2 f f 
+f 7 f f 8 f f 7 f f 8 f f 2 f f 
+f 7 f f f 8 8 8 8 8 f f f 2 f f 
+f 7 f f f f f 8 f f f f f 2 f f 
+f 7 f f f f f 8 f f f f f 2 f f 
+f f 7 7 7 7 7 8 2 2 2 2 2 f f f 
+f f f f 7 7 8 8 f f f f 2 f f f 
+f f f f 7 8 8 f f f f 2 2 f f f 
+f f f 7 7 8 f f f f 2 2 f f f f 
+f f f 7 8 8 f f 2 2 2 f f f f 7 
+7 7 7 7 8 2 2 2 2 f f f f f 7 7 
+`;
+            case "R9q{NEJjh0,]4*l5VF2-":
+            case "norsys2":return img`
+7 7 f f f f f f f f f f f f 7 7 
+7 f f f f 8 8 8 8 8 f f f f f 7 
+7 f f f 8 f f f f f 8 f f f f f 
+f f f f 8 f f f f f 8 f f f f f 
+f f 7 7 7 7 7 f 2 2 2 2 2 f f f 
+f 7 f f 8 f f 7 f f 8 f f 2 f f 
+f 7 f f 8 f f 7 f f 8 f f 2 f f 
+f 7 f f f 8 8 8 8 8 f f f 2 f f 
+f 7 f f f f f 8 f f f f f 2 f f 
+f 7 f f f f f 8 f f f f f 2 f f 
+f f 7 7 7 7 7 8 2 2 2 2 2 f f f 
+f f f f 7 7 8 8 f f f f 2 f f f 
+f f f f 7 8 8 f f f f 2 2 f f f 
+f f f 7 7 8 f f f f 2 2 f f f f 
+f f f 7 8 8 f f 2 2 2 f f f f 7 
+7 7 7 7 8 2 2 2 2 f f f f f 7 7 
+`;
+            case "u?7HKRdcozaPH$sSzqYM":
+            case "norsys3":return img`
+7 7 f f f f f f f f f f f f 7 7 
+7 f f f f 8 8 8 8 8 f f f f f 7 
+7 f f f 8 f f f f f 8 f f f f f 
+f f f f 8 f f f f f 8 f f f f f 
+f f 7 7 7 7 7 f 2 2 2 2 2 f f f 
+f 7 f f 8 f f 7 f f 8 f f 2 f f 
+f 7 f f 8 f f 7 f f 8 f f 2 f f 
+f 7 f f f 8 8 8 8 8 f f f 2 f f 
+f 7 f f f f f 8 f f f f f 2 f f 
+f 7 f f f f f 8 f f f f f 2 f f 
+f f 7 7 7 7 7 8 2 2 2 2 2 f f f 
+f f f f 7 7 8 8 f f f f 2 f f f 
+f f f f 7 8 8 f f f f 2 2 f f f 
+f f f 7 7 8 f f f f 2 2 f f f f 
+f f f 7 8 8 f f 2 2 2 f f f f 7 
+7 7 7 7 8 2 2 2 2 f f f f f 7 7 
+`;
+            case "nxUrB#1UVtSg*!)%efH}":
+            case "norsys4":return img`
+7 7 f f f f f f f f f f f f 7 7 
+7 f f f f 8 8 8 8 8 f f f f f 7 
+7 f f f 8 f f f f f 8 f f f f f 
+f f f f 8 f f f f f 8 f f f f f 
+f f 7 7 7 7 7 f 2 2 2 2 2 f f f 
+f 7 f f 8 f f 7 f f 8 f f 2 f f 
+f 7 f f 8 f f 7 f f 8 f f 2 f f 
+f 7 f f f 8 8 8 8 8 f f f 2 f f 
+f 7 f f f f f 8 f f f f f 2 f f 
+f 7 f f f f f 8 f f f f f 2 f f 
+f f 7 7 7 7 7 8 2 2 2 2 2 f f f 
+f f f f 7 7 8 8 f f f f 2 f f f 
+f f f f 7 8 8 f f f f 2 2 f f f 
+f f f 7 7 8 f f f f 2 2 f f f f 
+f f f 7 8 8 f f 2 2 2 f f f f 7 
+7 7 7 7 8 2 2 2 2 f f f f f 7 7 
+`;
+            case "26Mqx:,AECp5qTVPvOb}":
+            case "norsys5":return img`
+7 7 f f f f f f f f f f f f 7 7 
+7 f f f f 8 8 8 8 8 f f f f f 7 
+7 f f f 8 f f f f f 8 f f f f f 
+f f f f 8 f f f f f 8 f f f f f 
+f f 7 7 7 7 7 f 2 2 2 2 2 f f f 
+f 7 f f 8 f f 7 f f 8 f f 2 f f 
+f 7 f f 8 f f 7 f f 8 f f 2 f f 
+f 7 f f f 8 8 8 8 8 f f f 2 f f 
+f 7 f f f f f 8 f f f f f 2 f f 
+f 7 f f f f f 8 f f f f f 2 f f 
+f f 7 7 7 7 7 8 2 2 2 2 2 f f f 
+f f 7 f f f f 8 f f f f 2 f f f 
+f f 7 f f f 8 f f f f 2 2 f f f 
+f f 7 7 f f 8 8 f f 2 2 f f f f 
+f f f 7 7 f f 8 8 f 2 f f f f 7 
+f f f f 7 7 7 f 8 8 2 2 f f 7 7 
+`;
+            case "vfN~9ST8?P9rT^P}}mI^":
+            case "norsys6":return img`
+7 7 f f f f f f f f f f f f 7 7 
+7 f f f f 8 8 8 8 8 f f f f f 7 
+7 f f f 8 f f f f f 8 f f f f f 
+f f f f 8 f f f f f 8 f f f f f 
+f f 7 7 7 7 7 f 2 2 2 2 2 f f f 
+f 7 f f 8 f f 7 f f 8 f f 2 f f 
+f 7 f f 8 f f 7 f f 8 f f 2 f f 
+f 7 f f f 8 8 8 8 8 f f f 2 f f 
+f 7 f f f f f 8 f f f f f 2 f f 
+f 7 f f f f f 8 f f f f f 2 f f 
+f f 7 7 7 7 7 8 2 2 2 2 2 f f f 
+f f f 7 7 7 7 8 2 f f f f f f f 
+7 7 7 7 8 8 8 8 2 f f f f f f f 
+7 8 8 8 8 2 2 2 2 f f f f f f f 
+8 8 2 2 2 2 f f f f f f f f f 7 
+8 2 2 f f f f f f f f f f f 7 7 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Code généré automatiquement. Ne pas modifier.
